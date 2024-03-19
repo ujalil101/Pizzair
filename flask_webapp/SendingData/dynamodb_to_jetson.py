91b26e6ecca7394a413dcc89e5878e3c088d57b7
Usman Jalil <ujalil929@gmail.com>
2024-03-18 22:13:23 -0400

    added comments for db

--- flask_webapp/SendingData/dynamodb_to_jetson.py
+++ flask_webapp/SendingData/dynamodb_to_jetson.py
@@ -26,11 +26,10 @@ def fetch_from_dynamodb():
         response = dynamodb.scan(
             TableName=table_name
         )
-        
         # print items
-        for item in response['Items']:
-            print(item)
-
+        #for item in response['Items']:
+           # print(item) # this prints: {'DestinationLongitude': {'S': '42.3478109'}, 'StartingLatitude': {'S': '-71.1027253'}, 'user_loc': {'S': 'Sending'}, 'DestinationLatitude': {'S': '-71.1052321'}, 'StartingLongitude': {'S': '42.3503911'}}
+        return response['Items'] # just specify what you want.  look at geodata to dyanmodb to see formating
     except Exception as e:
         print("Error fetching data from DynamoDB:", e)
 
