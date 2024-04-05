a9483d6097a197556ee6f4d731f2941a29dd69e1
Usman Jalil <ujalil929@gmail.com>
2024-04-04 21:36:20 -0400

    restrucuted dynamo modules

--- flask_webapp/SendingData/geo_data_to_dynamodb.py
+++ flask_webapp/SendingData/geo_data_to_dynamodb.py
@@ -17,31 +17,42 @@ session = boto3.Session(
 dynamodb = session.client('dynamodb')
 
 
-
-
-def insert_into_dynamodb(starting_longitude, starting_latitude, destination_longitude, destination_latitude):
+def insert_into_dynamodb(destination, destination_latitude, destination_longitude, delivery_boolean):
     try:
-        session = boto3.Session(
-            aws_access_key_id=Access_Key,
-            aws_secret_access_key=Secret_Key,
-            region_name="us-east-1"
-        )
-        dynamodb = session.client('dynamodb')
-
         table_name = 'Coordinates'
-        response = dynamodb.put_item(
+        response = dynamodb.update_item(
             TableName=table_name,
-            Item={
-                'user_loc': {'S': 'Sending'},  
-                'StartingLatitude': {'S': str(starting_latitude)},
-                'StartingLongitude': {'S': str(starting_longitude)},
-                'DestinationLatitude': {'S': str(destination_latitude)},
-                'DestinationLongitude': {'S': str(destination_longitude)},
-            }
+            Key={
+                'user_loc': {'S': 'Admin'}  # use  same key value to ensure one item
+            },
+            UpdateExpression='SET DestinationLatitude = :lat, DestinationLongitude = :lon, Deliver = :deliv',
+            ExpressionAttributeValues={
+                ':lat': {'S': str(destination_latitude)},
+                ':lon': {'S': str(destination_longitude)},
+                ':deliv': {'BOOL': delivery_boolean}
+            },
+            ReturnValues='ALL_NEW'  # return updated item after the update
         )
-        print("Data inserted into DynamoDB:", response)
+        print("Data updated in DynamoDB:", response)
 
     except Exception as e:
-        print("Error inserting data into DynamoDB:", e)
+        print("Error updating data in DynamoDB:", e)
 
 
+def update_delivery_status(delivery_status):
+    try:
+        table_name = 'Coordinates'
+        key = {'user_loc': {'S': 'Admin'}}  # use same key value
+        response = dynamodb.update_item(
+            TableName=table_name,
+            Key=key,
+            UpdateExpression='SET Deliver = :deliv',
+            ExpressionAttributeValues={
+                ':deliv': {'BOOL': delivery_status}
+            },
+            ReturnValues='UPDATED_NEW'  # return the updated item after the update
+        )
+        print("Delivery status updated in DynamoDB:", response)
+
+    except Exception as e:
+        print("Error updating delivery status in DynamoDB:", e)
--- flask_webapp/app.py
+++ flask_webapp/app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for,session
 from SendingData.geocoding import geocode_address
 from dotenv import load_dotenv
-from SendingData.geo_data_to_dynamodb import insert_into_dynamodb
+from SendingData.geo_data_to_dynamodb import insert_into_dynamodb, update_delivery_status
 from ReceivingData.dynamo_to_app import retrieve_data  
-from SendingData.mapping_UI import Retrieve_Coordinates, fetch_from_dynamodb
 
 import boto3
 import os
@@ -22,45 +21,47 @@ def index():
 @app.route('/geocode', methods=['POST'])
 def geocode():
     data = request.get_json()
-    starting_point_coordinates = geocode_address(google_maps_api_key, data['startingPoint'])
     destination_coordinates = geocode_address(google_maps_api_key, data['destination'])
-    #  insert data into dyanmodb
-    insert_into_dynamodb(starting_point_coordinates[0], starting_point_coordinates[1], 
-                         destination_coordinates[0], destination_coordinates[1])
+    # update DynamoDB with delivery status
+    insert_into_dynamodb(data['destination'], destination_coordinates[0], destination_coordinates[1], True)
 
     return jsonify({
-        'startingPoint': starting_point_coordinates,
         'destination': destination_coordinates
     })
+
 
 @app.route('/submit_form', methods=['POST'])
 def submit_form(): 
     return redirect(url_for('display_data'))
 
 # This route will render the display_data.html page
-
-
 @app.route('/display_data')
 def display_data():
     # fetch data
     data = retrieve_data()
 
     # fetch coordinates
+    '''
+    
     dynamodb = Retrieve_Coordinates() 
     location = fetch_from_dynamodb(dynamodb)
     starting_lat = location[0]
     starting_lon = location[1]
     dest_lat = location[2]
     dest_lon = location[3]
-
+    '''
     # pass data and API key
     return render_template('display_data.html', 
-                           data=data, 
-                           starting_lat=starting_lat, 
-                           starting_lon=starting_lon,
-                           dest_lat=dest_lat,
-                           dest_lon=dest_lon,
-                           api_key=os.getenv('API_Key'))
+                           data=data)
+
+# Stop/Start Flight option
+@app.route('/update_delivery_status', methods=['POST'])
+def update_delivery_status_route():
+    data = request.get_json()
+    delivery_status = data.get('deliveryStatus')  # get the new delivery status from request
+    # update delivery status in Coordinates table
+    update_delivery_status(delivery_status)
+    return jsonify({'success': True})
     
 
 
